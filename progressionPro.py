7241fed71fade2f81e708481efbe3e7cb360c8bf
vimukthi waththegama <128338445+vimuktthi@users.noreply.github.com>
2023-11-20 17:16:14 +0530

    chnaged the variables

--- progressionPro.py
+++ progressionPro.py
@@ -11,18 +11,24 @@ def graphbar(win, x, y, width, height, color):  # function for creating a one ba
 def bargraph():  # function for generating the bar-graph
     global color
     win = GraphWin("Histogram", 750, 600)
-    outcomes = [pr, mt, mr, e]  # parameters those use to mark each outcome after entering data
-    outTotal = pr + mt + mr + e
+    outcomes = [progress, module_trailer, module_retriever, exclude]  # parameters those use to mark each outcome
+    # after entering data
+    outTotal = progress + module_trailer + module_retriever + exclude
     bartitles = ['Progress', 'Trailer', 'Retriever', 'Excluded']  # bar titles
     width = 100
     space = 40
-    maxH = max(outcomes)  # this use for building the bar graph not using values, using rate between the (value of
+    maxHeight = max(outcomes)  # this use for building the bar graph not using values, using rate between the (value of
     # outcome/maxH)
 
     for i, value in enumerate(outcomes):
+        start = Point(40, 521)
+        end = Point(560, 521)
+        bottom_line = Line(start, end)  # bottom line in bargraph
+        bottom_line.setOutline("black")
+        bottom_line.draw(win)
         x = i * (width + space) + space  # location of x-axis for each bar
         y = 520  # location of y-axis for all bars
-        height = (value / maxH) * 400  # this equation for height ,limited the height of bars
+        height = (value / maxHeight) * 400  # this equation for height ,limited the height of bars
         if bartitles[i] == "Progress":  # statement for giving a different different colors to each bar
             color = 'green'
         elif bartitles[i] == "Trailer":
@@ -44,16 +50,21 @@ def bargraph():  # function for generating the bar-graph
         bartitile = Text(Point(x + width / 2, y + 20), bartitles[i])
         bartitile.draw(win)
         bartop = Text(Point(x + width / 2, (y - height) - 10), outcomes[i])
-        bartop.draw(win)  # showing number of outcomes in top of the bar related to each outcome(pr,e,mt,mr)
+        bartop.draw(win)
 
-    win.getMouse()  # window will terminate after a mouse click on it, and programme will continue  with previous data
-    win.close()
+        # showing number of outcomes in top of the bar related to each outcome(pr,e,mt,mr)
+    try:
+        win.getMouse()  # window will terminate after a mouse click on it, and programme will continue  with previous
+        # data
+        win.close()
+    except GraphicsError:
+        pass
 
 
-e = 0  # Excluded                                   #variables use to mark(store) number of outcomes
-pr = 0  # Progress
-mt = 0  # Module trailer
-mr = 0  # Module retriever
+exclude = 0  # Excluded                                   #variables use to mark(store) number of outcomes
+progress = 0  # Progress
+module_trailer = 0  # Module trailer
+module_retriever = 0  # Module retriever
 
 outcomes = []  # list for store each generated outcome
 values = []  # this list use to store p,d,f values as a one character
@@ -64,8 +75,8 @@ while flag:  # nested while loops for get inputs repeatedly
     while flag:
         while True:
             try:  # programme won't crash when enter an invalid data type
-                p = int(input("Please enter your credits as pass:"))
-                if 0 <= p <= 120 and p % 20 == 0:  # checking that entered values are in valid range or not
+                pAss = int(input("Please enter your credits as pass:"))
+                if 0 <= pAss <= 120 and pAss % 20 == 0:  # checking that entered values are in valid range or not
                     break  # Exit the loop and, run it again when enter an invalid value
                 else:
                     print("Out of range")
@@ -73,8 +84,8 @@ while flag:  # nested while loops for get inputs repeatedly
                 print("Integer required")
         while True:
             try:
-                d = int(input("Please enter your credits as defer:"))
-                if 0 <= d <= 120 and d % 20 == 0:  # use and gate for checking using fewer codes
+                defer = int(input("Please enter your credits as defer:"))
+                if 0 <= defer <= 120 and defer % 20 == 0:  # use and gate for checking using fewer codes
                     break
                 else:
                     print("Out of range")
@@ -82,36 +93,37 @@ while flag:  # nested while loops for get inputs repeatedly
                 print("Integer required")
         while True:
             try:
-                f = int(input("Please enter your credits as fail:"))  # p,d & f are the variables for 3 inputs
-                if 0 <= f <= 120 and f % 20 == 0:
+                fail = int(input("Please enter your credits as fail:"))  # p,d & f are the variables for 3 inputs
+                if 0 <= fail <= 120 and fail % 20 == 0:
                     break
                 else:
                     print("Out of range")
             except ValueError:
                 print("Integer required")
-        total = p + d + f
-        variables = ((p, d, f),)  # get those p,d,f 3 values as a one variable
-        if total >= 120 and p % 20 == 0 and d % 20 == 0 and f % 20 == 0 and 0 <= p <= 120 and 0 <= d <= 120 and 0 <= f <= 120:
+        total = pAss + defer + fail
+        variables = ((pAss, defer, fail),)  # get those p,d,f 3 values as a one variable
+        if total >= 120 and pAss % 20 == 0 and defer % 20 == 0 and fail % 20 == 0 and \
+                0 <= pAss <= 120 and 0 <= defer <= 120 and 0 <= fail <= 120:
             values.extend(variables)  # checking again,because after enter invalid values those values will store in
             # p,d,f . then after checking, those values will store in 'values' list
         if total == 120:
             break  # programme will run till total equal to 120
         else:  # inputs repeatedly till this condition becomes true
             print("Total incorrect")
-    if f >= 80:
-        e = e + 1  # four conditional statements
+    if fail >= 80:
+        exclude = exclude + 1  # four conditional statements
         print("Exclude")
         outcomes.extend(("Exclude",))
-    elif p == 120:
-        pr = pr + 1  # marking(storing) number of outcomes
+    elif pAss == 120:
+        progress = progress + 1  # marking(storing) number of outcomes
         print("Progress")
         outcomes.extend(("Progress",))
-    elif p == 100:
-        mt = mt + 1  # for four outcomes
+    elif pAss == 100:
+        module_trailer = module_trailer + 1  # for four outcomes
         print("Progress (module trailer)")
         outcomes.extend(("Progress(Module trailer)",))
-    elif 60 <= p + d <= 120:
-        mr = mr + 1
+    elif 60 <= pAss + defer <= 120:
+        module_retriever = module_retriever + 1
         print("Do not progress – module retriever")
         outcomes.extend(("Module retriever",))
 
@@ -139,4 +151,4 @@ while flag:  # nested while loops for get inputs repeatedly
                 file = open('text.txt', 'r')  # and ope the file in read mode
             flag = False
         else:
-            print("Enter y or q!!!:") # enter an invalid character and jumps into the loop again
+            print("Enter y or q!!!:")  # enter an invalid character and jumps into the loop again
